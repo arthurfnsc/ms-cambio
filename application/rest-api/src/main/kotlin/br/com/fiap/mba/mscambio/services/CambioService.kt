4b1b3ed8255fc4c02994435a5ac7e830978d4091
Arthur Magalhaes Fonseca <arthurfnsc@gmail.com>
2020-09-06 13:12:56 -0300

    :sparkles: Implementing retrieve proposal state by uuid (#7)
    Implementing retrieve proposal state by uuid. Some fields were not mapped: statusTransacao, criadoEm, atualizadoEm

--- application/rest-api/src/main/kotlin/br/com/fiap/mba/mscambio/services/CambioService.kt
+++ application/rest-api/src/main/kotlin/br/com/fiap/mba/mscambio/services/CambioService.kt
@@ -1,19 +1,22 @@
 package br.com.fiap.mba.mscambio.services
 
+import br.com.fiap.mba.corda.states.PropostaState
 import br.com.fiap.mba.mscambio.dtos.EnvioPropostaDTO
+import net.corda.core.contracts.StateAndRef
 import net.corda.core.contracts.UniqueIdentifier
-import net.corda.core.messaging.CordaRPCOps
+import java.util.UUID
 
 interface CambioService {
 
     fun alterarStatusTransicao()
 
     fun enviarPropostaNegociacao(
-        propostaNegociacao: EnvioPropostaDTO,
-        proxy: CordaRPCOps
+        propostaNegociacao: EnvioPropostaDTO
     ): UniqueIdentifier
 
-    fun recuperarPropostaNegociacao()
+    fun recuperarPropostaNegociacao(
+        uuid: UUID?
+    ): StateAndRef<PropostaState>
 
     fun recuperarTransicoesDisponiveis()
 }
