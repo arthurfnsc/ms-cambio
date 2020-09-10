d2ac813bf253bb67b7c25318a5318e05718e983d
Arthur M Fonseca <arthurfnsc@gmail.com>
2020-09-10 03:53:32 -0300

    :white_check_mark: Adding ProposalContract test

---
+++ application/cordapp-contracts-states/src/test/kotlin/br/com/fiap/mba/corda/contracts/PropostaContractTest.kt
@@ -0,0 +1,172 @@
+package br.com.fiap.mba.corda.contracts
+
+import br.com.fiap.mba.corda.states.PropostaState
+import net.corda.core.identity.CordaX500Name
+import net.corda.testing.contracts.DummyContract
+import net.corda.testing.contracts.DummyState
+import net.corda.testing.core.DummyCommandData
+import net.corda.testing.core.TestIdentity
+import net.corda.testing.node.MockServices
+import net.corda.testing.node.ledger
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+import java.time.Instant
+
+class PropostaContractTest {
+
+    private val ledgerServices = MockServices(
+        listOf("br.com.fiap.mba.corda.contracts", "net.corda.testing.contracts")
+    )
+
+    private val alice = TestIdentity(CordaX500Name("alice", "New York", "US"))
+    private val bob = TestIdentity(CordaX500Name("bob", "Tokyo", "JP"))
+    private val charlie = TestIdentity(CordaX500Name("charlie", "London", "GB"))
+
+    private val propostaState = PropostaState(
+        statusTransacao = "ABERTO",
+        comprador = alice.party,
+        proponente = alice.party,
+        vendedor = bob.party,
+        oblato = bob.party,
+        moeda = "USD",
+        quantidade = 1,
+        cotacaoReal = BigDecimal.ONE,
+        taxa = BigDecimal.TEN
+    )
+
+    private val propostaStateInvalida1 = PropostaState(
+        statusTransacao = "ABERTO",
+        comprador = alice.party,
+        proponente = bob.party,
+        vendedor = bob.party,
+        oblato = alice.party,
+        moeda = "USD",
+        quantidade = 1,
+        cotacaoReal = BigDecimal.ONE,
+        taxa = BigDecimal.TEN
+    )
+
+    private val propostaStateInvalida2 = PropostaState(
+        statusTransacao = "ABERTO",
+        comprador = alice.party,
+        proponente = alice.party,
+        vendedor = bob.party,
+        oblato = charlie.party,
+        moeda = "USD",
+        quantidade = 1,
+        cotacaoReal = BigDecimal.ONE,
+        taxa = BigDecimal.TEN
+    )
+
+    private val propostaStateInvalida3 = PropostaState(
+        statusTransacao = "ABERTO",
+        comprador = alice.party,
+        proponente = charlie.party,
+        vendedor = bob.party,
+        oblato = bob.party,
+        moeda = "USD",
+        quantidade = 1,
+        cotacaoReal = BigDecimal.ONE,
+        taxa = BigDecimal.TEN
+    )
+
+    @Test
+    fun `deve possuir um output do tipo PropostaState`() {
+        ledgerServices.ledger {
+            transaction {
+                command(listOf(alice.publicKey, bob.publicKey), NegociacaoContract.Commands.Proposta())
+                fails()
+                tweak {
+                    NegociacaoContract.ID?.let { output(it, DummyState()) }
+                    fails()
+                }
+                tweak {
+                    NegociacaoContract.ID?.let { output(it, propostaState) }
+                    NegociacaoContract.ID?.let { output(it, propostaState) }
+                    fails()
+                }
+                NegociacaoContract.ID?.let { output(it, propostaState) }
+                verifies()
+            }
+        }
+    }
+
+    @Test
+    fun `deve possuir uma transação de proposta e ter exatamente um comando do tipo Proposta`() {
+        ledgerServices.ledger {
+            transaction {
+                NegociacaoContract.ID?.let { output(it, propostaState) }
+                tweak {
+                    command(listOf(alice.publicKey, bob.publicKey), DummyCommandData)
+                    fails()
+                }
+                command(listOf(alice.publicKey, bob.publicKey), NegociacaoContract.Commands.Proposta())
+                verifies()
+            }
+        }
+    }
+
+    @Test
+    fun `deve possuir 2 assinadores obigatórios - proponente e oblato`() {
+        ledgerServices.ledger {
+            transaction {
+                NegociacaoContract.ID?.let { output(it, propostaState) }
+                tweak {
+                    command(listOf(alice.publicKey, charlie.publicKey), NegociacaoContract.Commands.Proposta())
+                    fails()
+                }
+                tweak {
+                    command(listOf(charlie.publicKey, bob.publicKey), NegociacaoContract.Commands.Proposta())
+                    fails()
+                }
+                command(listOf(alice.publicKey, bob.publicKey), NegociacaoContract.Commands.Proposta())
+                verifies()
+            }
+        }
+    }
+
+    @Test
+    fun `deve validar transações em que comprador e vendedor são proponente e oblato respectivamente`() {
+        ledgerServices.ledger {
+            transaction {
+                command(listOf(alice.publicKey, bob.publicKey), NegociacaoContract.Commands.Proposta())
+                tweak {
+                    // Order reversed - buyer = proposee, seller = proposer
+                    NegociacaoContract.ID?.let { output(it, propostaStateInvalida1) }
+                    verifies()
+                }
+                tweak {
+                    NegociacaoContract.ID?.let { output(it, propostaStateInvalida2) }
+                    fails()
+                }
+                tweak {
+                    NegociacaoContract.ID?.let { output(it, propostaStateInvalida3) }
+                    fails()
+                }
+                NegociacaoContract.ID?.let { output(it, propostaState) }
+                verifies()
+            }
+        }
+    }
+
+    @Test
+    fun `deve validar propostas sem input e sem timestamp`() {
+        ledgerServices.ledger {
+            transaction {
+                NegociacaoContract.ID?.let { output(it, propostaState) }
+                command(
+                    listOf(alice.publicKey, bob.publicKey),
+                    NegociacaoContract.Commands.Proposta()
+                )
+                tweak {
+                    input(DummyContract.PROGRAM_ID, DummyState())
+                    fails()
+                }
+                tweak {
+                    timeWindow(Instant.now())
+                    fails()
+                }
+            }
+        }
+    }
+}
